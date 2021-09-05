3a276cdad9695cae8aca82d21cefd0c68051fcf0
YaLlama <47946874+YaLlama@users.noreply.github.com>
2021-09-05 12:12:12 -0400

    Wokring Cube

--- CompSciTA/Main.py
+++ CompSciTA/Main.py
@@ -5,31 +5,61 @@ import Display
 import time
 
 background_colour = (255,255,255)
-(width, height) = (900, 700)
+(width, height) = (1600, 800)
 forestGreen = (34, 139, 34)
 
 pygame.init()
 clock = pygame.time.Clock()
 display = displayControler2.display(pygame.display, width, height)
-shapes = ObjectControl.objectControl(display)
+shapes = ObjectControl.objectControl(display,width,height)
 
 
 running = True
 
+faces = [[[152, 147], [247, 147], [152, 52], [247, 52]], [[139, 160], [260, 160], [139, 39], [260, 39]], [[139, 160], [260, 160], [152, 147], [247, 147]], [[139, 39], [260, 39], [152, 52], [247, 52]], [[139, 160], [139, 39], [152, 147], [152, 52]], [[260, 160], [260, 39], [247, 147], [247, 52]]]
+
+face1 = [[152, 147], [247, 147], [152, 52], [247, 52]]
+face2 = [[139, 160], [260, 160], [139, 39], [260, 39]]
+face3 = [[139, 160], [260, 160], [152, 147], [247, 147]]
+face4 = [[139, 39], [260, 39], [152, 52], [247, 52]]
+face5 = [[139, 160], [139, 39], [152, 147], [152, 52]]
+face6 = [[260, 160], [260, 39], [247, 147], [247, 52]]
+
 
 
 count = 2
+shapes.regularShape(width/2, height/2, 200, count, (0, 0, 210))
+display.update()
+count += 1
+
+
+camera = [0, 0, 0]
+
+
 
 while running:
 
-    shapes.regularShape(width/2, height/2, 200, count, (0, 0, 210))
+    shapes.cube(camera)
     display.update()
-    count += 1
+
     clock.tick(30)
 
-
-
-
     for event in pygame.event.get():
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_w:
+                camera[1] += 1
+            if event.key == pygame.K_s:
+                camera[1] -= 1
+            if event.key == pygame.K_a:
+                camera[0] -= 1
+            if event.key == pygame.K_d:
+                camera[0] += 1
+            if event.key == pygame.K_q:
+                camera[2] += 1
+            if event.key == pygame.K_e:
+                camera[2] -= 1
+
         if event.type == pygame.QUIT:
           running = False
--- CompSciTA/ObjectControl.py
+++ CompSciTA/ObjectControl.py
@@ -2,10 +2,12 @@ import math
 
 class objectControl:
 
-    def __init__(self, disclass):
-        global display
+    def __init__(self, d, w, h):
+        global display, width, height
 
-        display = disclass
+        display = d
+        height = h
+        width = w
 
     def regularShape(self, X, Y, radius, sides, color):
         points = []
@@ -19,4 +21,79 @@ class objectControl:
 
         display.drawShape(points, color)
 
+    def draw3DShape(self):
+        #side is defined as starting at one point and ending at same point, or with an indicator
+        pass
 
+    def cube(self, camera):
+
+        points3D = [[-5, 5, 18], [5, 5, 18], [-5, -5, 18], [5, -5, 18], [-5, 5, 23], [5, 5, 23], [-5, -5, 23], [5, -5, 23]]
+
+        face1 = [[5, 5, 18], [-5, 5, 18], [-5, -5, 18], [5, -5, 18]]
+        face2 = [[5, 5, 23], [-5, 5, 23], [-5, -5, 23], [5, -5, 23]]
+        face3 = [[5, 5, 18], [-5, 5, 18], [-5, 5, 23], [5, 5, 23]]
+        face4 = [[5, -5, 18], [-5, -5, 18], [-5, -5, 23], [5, -5, 23]]
+        face5 = [[-5, -5, 18], [-5, 5, 18], [-5, 5, 23], [-5, -5, 23]]
+        face6 = [[5, -5, 18], [5, 5, 18], [5, 5, 23], [5, -5, 23]]
+
+        faces = [face1, face2, face3, face4, face5, face6]
+
+        faces2D = []
+        faces2DDists = []
+
+        for face in faces:
+
+            face2D = self.EDto2D(face, camera)
+
+            faceDist = face2D[len(face2D)-1]
+            face2D.remove(faceDist)
+
+            i = len(faces2D)
+
+            for n in range(len(faces2DDists)):
+                #want to be ordered (furthest) largest (closest point) to shortest
+
+                dist = faces2DDists[n]
+                if faceDist > dist:
+                    i = n
+
+            faces2DDists.insert(i, faceDist)
+            faces2D.insert(i, face2D)
+
+        for face in faces2D:
+            display.drawShape(face, (0, 0, 230))
+
+
+
+    def EDto2D(self, points3D, camera):
+
+        points2D = []
+        closestPointdist = 99999999999
+
+        fov = 103
+        fov = math.radians(fov)
+        yFov = fov * height / width
+
+
+
+        for i in range(len(points3D)):
+
+            point = points3D[i]
+
+            xDist = point[0] - camera[0]
+            yDist = point[1] - camera[1]
+            zDist = point[2] - camera[2]
+
+            y = height * (yFov/2 + math.atan(yDist / zDist)) / yFov
+            x = width * (fov/2 + math.atan(xDist / zDist)) / fov
+
+            dist = math.sqrt(yDist * yDist + xDist * xDist + zDist * zDist)
+
+            if dist < closestPointdist:
+                closestPointdist = dist
+
+            points2D.append([int(x), int(y)])
+
+        points2D.append(closestPointdist)
+
+        return points2D
